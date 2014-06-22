81f16a4b90b2c44a5309b1bb7a97435669148598
Scotty Vernon <scott.vernon@gmail.com>
2014-06-22 19:36:11 +0100

    Add more tests.

--- testing/cow_test.js
+++ testing/cow_test.js
@@ -1,10 +1,28 @@
 var expect = chai.expect;
 
-describe("Cow", function() {
-  describe("constructor", function() {
-    it ("should have a default name", function() {
+describe("Cow", function () {
+  describe("constructor", function () {
+    it ("should have a default name", function () {
       var cow = new Cow();
       expect(cow.name).to.equal("Anon the cow");
     });
-  })
+
+    it ("should set a cow's name if a name is provided", function () {
+      var cow = new Cow("Geoff");
+      expect(cow.name).to.equal("Geoff");
+    });
+  });
+
+  describe("#greets", function () {
+    it ("should throw if no target is passed in", function () {
+      expect (function() {
+        (new Cow()).greets();
+      }).to.throw(Error);
+    });
+
+    it ("should greet passed target", function () {
+      var greetings = (new Cow("Geoff")).greets("Mr Farmer");
+      expect(greetings).to.equal("Geoff greets Mr Farmer");
+    });
+  });
 })
