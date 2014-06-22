e8fcf817c3b01b203e8832e1f47a6eae431750ac
Scotty Vernon <scott.vernon@gmail.com>
2014-06-22 19:54:46 +0100

    Scaffolding basket js test example.

---
+++ testing/basket.js
@@ -0,0 +1,22 @@
+(function (exports) {
+  "use strict";
+
+  var cart = {
+
+    items : 0,
+    total : 0,
+
+    addToBasket : function() {
+
+    },
+
+    removeFromBasket : function() {
+
+    },
+
+    updateItemQuantity : function() {
+
+    }
+  }
+
+})(this)
---
+++ testing/basket_test.js
@@ -0,0 +1,14 @@
+var expect = chai.expect;
+
+describe("Basket", function () {
+  describe("logic", function () {
+    it ("should add item to basket ", function () {
+    });
+
+    it ("should remove item from basket", function () {
+    });
+
+    it ("should update item quantity", function() {
+    });
+  })
+})
